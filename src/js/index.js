aa1ea06d5e05282dc7468045f9bff3bed63de30a
Adán de la Rosa Lugo <alu0100909012@ull.edu.es>
2021-10-05 00:10:47 +0100

    Refactoring code and adding attacks.

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 const pikachu = {
     name: "pikachu",
     health: 100,
-    atacks:  {
+    attacks:  {
         thunder: 20,
         tackle: 5
     }
@@ -9,38 +9,57 @@ const pikachu = {
 const charmander = {
     name: "charmander",
     health: 120,
-    atacks:  {
+    attacks:  {
         flamethrower: 20,
         blaze: 30
     }
 };
+
 const start = btnStart.addEventListener( 'click', () => {
-    const playerHealth = pikachu.health;
-    const enemyHealth = charmander.health;
-    // Vidas y boton de inicio
-    const startButton = document.querySelector('#btnStart');
-    const playerHealthBox = document.querySelector('#player-health');
-    const enemyHealthBox = document.querySelector('#enemy-health');
-    const playerHealthDiv = document.createElement(`div`);
-    const enemyHealthDiv = document.createElement(`div`);
+    const startButton = document.querySelector('#btnStart')
+    startButton.classList.toggle('hide');
+    createPokemon(pikachu, true);
+    createPokemon(charmander, false);  
+});
 
-    playerHealthDiv.textContent = `Hp: ${playerHealth}`;
-    playerHealthBox.appendChild(playerHealthDiv);
-    enemyHealthDiv.textContent = `Hp: ${enemyHealth}`;
-    enemyHealthBox.appendChild(enemyHealthDiv);
-    startButton.classList.toggle('btn-start-hide');
-    // Ataques
-    const attacksBox = document.querySelector('.attacks');
-    const attackBtn1 = document.createElement(`button`);
-    const attackBtn2 = document.createElement(`button`);
-    const playerAttacks = Object.keys(pikachu.atacks);
+const createPokemon = (pokemon, isPlayer) => {
+    const pokemonHealth = pokemon.health;
 
-    attackBtn1.textContent = `${playerAttacks[0]}`;
-    attackBtn2.textContent = `${playerAttacks[1]}`;
-    attacksBox.appendChild(attackBtn1);
-    attacksBox.appendChild(attackBtn2);
+    if (isPlayer) {
+        const playerHealthBox = document.querySelector('#player-health');
+        const playerHealthDiv = document.createElement(`div`);
 
+        playerHealthDiv.textContent = `Hp: ${pokemonHealth}`;
+        playerHealthBox.appendChild(playerHealthDiv);
+        createAttacks(pokemon);
+    }
+    else {
+        const enemyHealthBox = document.querySelector('#enemy-health');
+        const enemyHealthDiv = document.createElement(`div`);
     
+        enemyHealthDiv.textContent = `Hp: ${pokemonHealth}`;
+        enemyHealthBox.appendChild(enemyHealthDiv);
+
+    }
+};
+const createAttacks = (pokemon) => {
+    const attacksBox = document.querySelector('.attacks');
+    const playerAttacks = Object.keys(pokemon.attacks);
+    playerAttacks.forEach((attack, index) => {
+        const attackButton = document.createElement(`button`);
+        attackButton.classList.add(`attackButton`)
+        attackButton.textContent = `${index + 1}) ${attack}`;
+        attacksBox.appendChild(attackButton);
+
+    });
+    
+
+};
+
+const attackMovement = document.querySelector('.attacks').addEventListener( 'click', (event) => {
+    if(event.target.className == "attackButton")
+        console.log("Me ejecuto");
+        
 });
 
 
